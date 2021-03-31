81f500f2a6d7c450596b47d4c0e318ce900d2da6
Naim <mail@thenaim.com>
2021-03-31 15:06:52 +0200

    update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,12 @@
 # Set the variable value in *.tfvars file
+# or export TF_VAR_do_token=""
 # or using -var="do_token=..." CLI option
 variable "do_token" {}
 
 # Set droplet count
 variable "droplet_count" {
   type    = number
-  default = 1
+  default = 3
 }
 
 # Set droplet image
